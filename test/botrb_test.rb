871a23302e3ab897e88b81da59087490c2f73e0d
Jeff Rogers <jbrogers63@gmail.com>
2019-01-22 17:08:28 -0500

    Setup bot init method and some basic tests. Drop boilerplate test.

--- test/botrb_test.rb
+++ test/botrb_test.rb
@@ -1,11 +1,28 @@
 require 'test_helper'
 
 class BotrbTest < Minitest::Test
+  attr_accessor :server, :bot
+
+  def setup_bot
+    config = {
+      name: 'Botrb'
+    }
+    @bot = Botrb::Bot.new config
+    @server, @bot.socket = IO.pipe
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Botrb::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_bot_setup
+    setup_bot
+    assert @bot.name.eql? 'Botrb'
+  end
+
+  def test_bot_io_methods
+    setup_bot
+    @bot.say 'Hello, there!'
+    assert server.gets.chomp!.eql? 'Hello, there!'
   end
 end
